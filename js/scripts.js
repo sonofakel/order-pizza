b63273317349f8f707a2bbf9663fa9e96a919496
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-21 09:12:03 -0700

    add bus and ui logic to make spec 1 work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
 // Business Logic -->
 function Pizza(size, topping) {
   this.size = size;
-  this.topping = new Topping();
+  // this.topping = new Topping();
 }
 
-function Topping(name) {
-  this.name = name;
-  this.price = 1;
-}
+// function Topping(name) {
+//   this.name = name;
+//   this.price = 1;
+// }
 
 
 
 
 
 // UI Logic -->
+$(function(){
+  $("#select-options").submit(function(event){
+    event.preventDefault();
+
+    var size = $("input:radio[name=size]:checked").val();
+
+    var newPizza = new Pizza(size)
+
+    $("#output").text(size);
+
+
+  })
+})
