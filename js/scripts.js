97a8a4dc519ee16b136a33f37c9018fc25e709cb
Charlie Kelson <charlie.m.kelson@gmail.com>
2017-07-21 09:02:17 -0700

    add  rest of checkboxes and submit button and start on business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 // Business Logic -->
+function Pizza(size, topping) {
+  this.size = size;
+  this.topping = new Topping();
+}
 
-
-
-
-
-
+function Topping(name) {
+  this.name = name;
+  this.price = 1;
+}
 
 
 
